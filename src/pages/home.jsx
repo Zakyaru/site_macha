a7adf0b3ae683ba456416a84318302dea1c881d2
Zakyaru <arseni.ergin@gmail.com>
2025-04-20 15:08:35 +0200

    test de push

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+        <h1 className="text-3xl font-bold underline">Hello World Worlmd!</h1>
     );
 }
 
